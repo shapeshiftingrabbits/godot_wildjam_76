c49be8ebfbda2689af3159f4cf669abfb0226139
Laurie Trichet <laurie.trichet@protonmail.com>
2024-12-16 16:47:19 +1100

    player movement state machine works

--- scripts/handle_state_chart.gd
+++ scripts/handle_state_chart.gd
@@ -3,11 +3,20 @@ extends Node
 class_name HandleStateChart
 
 @onready var state_chart: StateChart = $"../StateChart"
+@onready var player: Player = %Player
 
-
+func _ready() -> void:
+	#await get_tree().create_timer(0.1).timeout
+	await state_chart.ready
+	state_chart.set_expression_property(&"shoved_back", false)
+	
+	
 func _on_move_character_back_player_is_moved_back() -> void:
 	state_chart.set_expression_property(&"shoved_back", true)
 
 
 func _on_move_character_back_end_move_back() -> void:
 	state_chart.set_expression_property(&"shoved_back", false)
+
+func _physics_process(delta: float) -> void:
+	state_chart.set_expression_property(&"velocity", player.velocity)
