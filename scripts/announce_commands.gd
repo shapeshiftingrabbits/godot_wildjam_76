51b7bbf8fc168b2bfa7bb171bab6756385f1ce62
Laurie Trichet <laurie.trichet@protonmail.com>
2024-12-19 16:39:34 +1100

    replaced music with VO

--- scripts/announce_commands.gd
+++ scripts/announce_commands.gd
@@ -2,8 +2,16 @@ extends Node
 
 class_name AnnounceCommands
 
-@onready var timer: Timer = $Timer
 @onready var state_chart: StateChart = $"../StateChart"
+
+@onready var first_timer: Timer = $FirstTimer
+@onready var second_timer: Timer = $SecondTimer
+@onready var third_timer: Timer = $ThirdTimer
+@onready var soleil_timer: Timer = $SoleilTimer
+@onready var check_timer: Timer = $CheckTimer
+
+#@onready var game_timers: Array[Timer] = [first_timer, second_timer, third_timer, soleil_timer]
+#@onready var all_timers: Array[Timer] = game_timers + [check_timer]
 
 
 func _on_detect_starting_line_reached_entered_play_area() -> void:
@@ -11,19 +19,15 @@ func _on_detect_starting_line_reached_entered_play_area() -> void:
 
 
 func reset_game():
-	timer.stop()
+	#all_timers.map(func (timer): timer.stop())
 	state_chart.send_event(&"idled")
 
 
 func start_game():
 	state_chart.send_event(&"started")
-	timer.start()
-
-
-func _on_timer_timeout() -> void:
-	state_chart.send_event(&"next")
+	#game_timers.map(func (timer): timer.start())
 
 
 func _on_detect_finish_line_reached_finished_line_reached() -> void:
-	timer.stop()
+	#all_timers.map(func (timer): timer.stop())
 	state_chart.send_event(&"ended")
--- scripts/check_player_movement.gd
+++ scripts/check_player_movement.gd
@@ -7,10 +7,10 @@ signal player_has_moved
 @onready var player: CharacterBody2D = %Player
 
 
-func _on_stop_state_state_physics_processing(delta: float) -> void:
-	if !is_player_frozen():
-		player_has_moved.emit()
-
-
 func is_player_frozen() -> bool:
 	return player.velocity == Vector2.ZERO
+
+
+func _on_check_state_state_physics_processing(delta: float) -> void:
+	if !is_player_frozen():
+		player_has_moved.emit()
