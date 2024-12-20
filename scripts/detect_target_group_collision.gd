6a90cc768eb9c7f766184bb3d35b2ae50c221005
Laurie Trichet <laurie.trichet@protonmail.com>
2024-12-20 19:04:59 +1100

    add a way to disable the exit trigger

--- scripts/detect_target_group_collision.gd
+++ scripts/detect_target_group_collision.gd
@@ -6,6 +6,13 @@ signal target_group_entered
 
 @export var target_group_name: StringName = &"player"
 
+@onready var collision_shape_2d: CollisionShape2D = $CollisionShape2D
+
+@export var disabled: bool = false
+
+func _ready() -> void:
+	collision_shape_2d.set_deferred("disabled", disabled)
+	
 
 func _on_body_entered(body: Node2D) -> void:
 	if body.is_in_group(target_group_name):
