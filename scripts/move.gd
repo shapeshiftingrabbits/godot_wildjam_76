e42954201558bfc516b8a50d6778d794983d6876
Laurie Trichet <laurie.trichet@protonmail.com>
2024-12-20 11:35:09 +1100

    add sfx for hit and jump

--- scripts/move.gd
+++ scripts/move.gd
@@ -20,6 +20,7 @@ func _on_in_control_state_state_physics_processing(delta: float) -> void:
 	# Handle jump.
 	if Input.is_action_just_pressed("jump") and is_on_floor():
 		velocity.y = JUMP_VELOCITY
+		jumping.emit()
 
 	# Get the input direction and handle the movement/deceleration.
 	# As good practice, you should replace UI actions with custom gameplay actions.
