5b9dbf5b8912d487d5d464b1adb204b6f58685fa
Laurie Trichet <laurie.trichet@protonmail.com>
2024-12-22 10:39:37 +1100

    add reloading of worlds

--- scripts/menu_controller.gd
+++ scripts/menu_controller.gd
@@ -6,7 +6,7 @@ func _ready() -> void:
 
 
 func _on_exit_to_main_screen_button_pressed() -> void:
-	get_tree().reload_current_scene()
+	get_tree().change_scene_to_file("res://scenes/main_scene.tscn")
 
 
 func _on_quit_button_pressed() -> void:
@@ -33,3 +33,7 @@ func _input(event: InputEvent) -> void:
 func resume() -> void:
 	hide()
 	get_tree().paused = false
+
+
+func _on_reload_current_world_pressed() -> void:
+	get_tree().reload_current_scene()
